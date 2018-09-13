805c37b6f0a8c20451c101fb679f4b0b076d2736
SergioRt <sergio200035@gmail.com>
2018-09-13 18:14:29 -0500

    trabajo en clase

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -1,81 +1,92 @@
+var controller = (function () {
 
-var url = '';
+    var url = '';
 
-function getOrders() {
-    return axios.get(url+"/orders").then(function (response) {
-    return response.data;
-  })
-}
-
-function getProducts() {
-    return axios.get(url+"/orders/products").then(function (response) {
-    return response.data;
-  })
-}
-
-function clearTables(){
-    var tables = document.getElementsByClassName("table"); 
-    for (i = 0; i < tables.length; i++) {
-        tables[i].innerHTML = "";
+    function getOrders() {
+        return axios.get(url + "/orders").then(function (response) {
+            return response.data;
+        })
     }
-}
 
-function addOrder(order) {
-    var tableOrder = document.getElementById("tableOrder");
-    var header = document.createElement("tr");
-    var cell = document.createElement("th");
-    cell.innerHTML = "Product";
-    header.appendChild(cell);
-    var cell = document.createElement("th");
-    cell.innerHTML = "Quantity";
-    header.appendChild(cell);
-    var cell = document.createElement("th");
-    cell.innerHTML = "Price";
-    header.appendChild(cell);
-    tableOrder.appendChild(header);
-    tableOrder.setAttribute("class","table");
-    for (var i = 0; i < order.products.length; i++) {
-        var row = document.createElement("tr");
-
-        var cell = document.createElement("td");
-        cell.innerHTML = order.products[i].product;
-        row.appendChild(cell);
-
-        var cell = document.createElement("td");
-        cell.innerHTML = order.products[i].quantity;
-        row.appendChild(cell);
-
-        var cell = document.createElement("td");
-        cell.innerHTML = order.products[i].price;
-        row.appendChild(cell);
-
-        tableOrder.appendChild(row);
+    function getProducts() {
+        return axios.get(url + "/orders/products").then(function (response) {
+            return response.data;
+        })
     }
-}
 
-function loadOrders(){
-    clearTables();
-    loadData(orderBuilder);
-}
+    function clearTables() {
+        var tables = document.getElementsByClassName("table");
+        for (i = 0; i < tables.length; i++) {
+            tables[i].innerHTML = "";
+        }
+    }
 
-function loadData(callback){
-    axios.all([getOrders(),getProducts()])
-            .then(axios.spread(function (orders, products){
-                callback(orders,products);
-        }));
-}
+    function addOrder(order) {
+        var tableOrder = document.getElementById("tableOrder");
+        var header = document.createElement("tr");
+        var cell = document.createElement("th");
+        cell.innerHTML = "Product";
+        header.appendChild(cell);
+        var cell = document.createElement("th");
+        cell.innerHTML = "Quantity";
+        header.appendChild(cell);
+        var cell = document.createElement("th");
+        cell.innerHTML = "Price";
+        header.appendChild(cell);
+        tableOrder.appendChild(header);
+        tableOrder.setAttribute("class", "table");
+        for (var i = 0; i < order.products.length; i++) {
+            var row = document.createElement("tr");
 
-function orderBuilder(orders ,products){
-    for(i in orders){
-        var orderJson = orders[i];
-        var order = {order_id:orderJson.tableNumber, table_id:orderJson.tableNumber, products:[]};
-        for(productName in orderJson.orderAmountsMap){
-            var prod = {"product": productName ,"quantity": orderJson.orderAmountsMap[productName],"price": null}
-            prod.price = products.filter(function (x){
+            var cell = document.createElement("td");
+            cell.innerHTML = order.products[i].product;
+            row.appendChild(cell);
+
+            var cell = document.createElement("td");
+            cell.innerHTML = order.products[i].quantity;
+            row.appendChild(cell);
+
+            var cell = document.createElement("td");
+            cell.innerHTML = order.products[i].price;
+            row.appendChild(cell);
+
+            tableOrder.appendChild(row);
+        }
+    }
+
+    function loadOrders() {
+        clearTables();
+        loadData(ordersBuilder);
+    }
+
+    function loadData(callback) {
+        axios.all([getOrders(), getProducts()])
+                .then(axios.spread(function (orders, products) {
+                    callback(orders, products);
+                }));
+    }
+
+
+
+    function ordersBuilder(orders, products) {
+        for (i in orders) {
+            orderBuilder(orders[i], products)
+        }
+    }
+
+    function orderBuilder(orderJson, products) {
+        var order = {order_id: orderJson.tableNumber, table_id: orderJson.tableNumber, products: []};
+        for (productName in orderJson.orderAmountsMap) {
+            var prod = {"product": productName, "quantity": orderJson.orderAmountsMap[productName], "price": null}
+            prod.price = products.filter(function (x) {
                 return x.name === productName;
             })[0].price;
             order.products.push(prod);
         }
         addOrder(order);
     }
-}
+    return {
+        loadOrders: loadOrders,
+        getOrders: getOrders
+    };
+})();
